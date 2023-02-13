00dc9a3a17d589117d3ab8becb7bd26761dd0cb8
Zwelisha Langa <zwelisha@bbd.co.za>
2023-02-13 22:19:53 +0200

    Removed the class variable pathToFile as it did not belong in the IDChecker class

--- IDValidator.cs
+++ IDValidator.cs
@@ -7,17 +7,10 @@ namespace Validator
         private readonly int EvalutedYear;
         private int OldYears = 0;
         private int NewYears = 0;
-        private string pathToFile;
         private string[] listOfIdStrings = new string[] { };
 
-        public string PathToFile
-        {
-            get { return pathToFile; }
-            set { pathToFile = value; }
-        }
         public string[] ListOfIdStrings
         {
-            get { return listOfIdStrings; }
             set { listOfIdStrings = value; }
         }
 
@@ -44,6 +37,80 @@ namespace Validator
         public int ReturnSumOfNewIdentities()
         {
             return NewYears;
+        }
+
+        public void EvaluateIdentidities()
+        {
+            foreach (string IdStrings in listOfIdStrings)
+            {
+                // Check if ID is valid
+                if (idChecker(IdStrings))
+                {
+                    Console.WriteLine(createDate(IdStrings));
+                    if (before2010(IdStrings))
+                    {
+                        IdentifiedAsOldID();
+                    }
+                    else
+                    {
+                        IdentifiedAsNewID();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("ERROR: " + IdStrings);
+                }
+            }
+        }
+
+        static bool idChecker(string idString)
+        {
+            // Check length
+            bool isCorrectLength = idString.Length == 13;
+
+            // Check if all characters are digits
+            bool isAllNumbers = false;
+
+            foreach (char item in idString)
+            {
+                isAllNumbers = Char.IsDigit(item);
+            }
+
+            // Check if month is correct
+            bool isMonthCorrect = false;
+
+            int month = Int32.Parse(idString.Substring(2, 2));
+            isMonthCorrect = 0 < month && month <= 12;
+
+            // Check if day is correct
+            bool isDayCorrect = false;
+
+            int day = Int32.Parse(idString.Substring(4, 2));
+            isDayCorrect = 0 < day && day <= 31;
+
+            return isCorrectLength && isAllNumbers && isMonthCorrect && isDayCorrect;
+        }
+
+        static string createDate(string IdNumber)
+        {
+            string year = IdNumber.Substring(0, 2);
+            string month = IdNumber.Substring(2, 2);
+            string day = IdNumber.Substring(4, 2);
+
+            string dateString = day + "/" + month + "/" + year;
+
+            return dateString;
+        }
+
+        static bool before2010(string idString)
+        {
+            int year = Int32.Parse(idString.Substring(0, 2));
+
+            if (year > 23 || year < 10)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,73 +7,15 @@ namespace filereader
    class Program{
       static void Main(string[] args){
          string pathToFile = @"C:\Users\bbdnet2866\BBD_Training\C#\filereader\ids.txt";
+         
          IDChecker checker = new IDChecker(2010);
-         checker.PathToFile = pathToFile;
 
-         checker.ListOfIdStrings = InputOuputReader.GetFileContent(checker.PathToFile);
+         checker.ListOfIdStrings = InputOuputReader.GetFileContent(pathToFile);
 
-         foreach (string IdStrings in checker.ListOfIdStrings)
-            {
-            // Check if ID is valid
-            if (idChecker(IdStrings)) {
-               Console.WriteLine(createDate(IdStrings));
-               if (before2010(IdStrings)) {
-                    checker.IdentifiedAsOldID();
-               } else {
-                    checker.IdentifiedAsNewID();
-                }
-            } else {
-               Console.WriteLine("ERROR: "+ IdStrings);
-            }
-         }
+         checker.EvaluateIdentidities();
 
          string writeText = "Number of people before 2010: "+ checker.ReturnSumOfOldIdentities()+ "\n"+"Number of people after 2010: "+checker.ReturnSumOfNewIdentities();
          System.IO.File.WriteAllText("result.txt", writeText);
-      }
-
-      static bool idChecker(string idString) {
-         // Check length
-         bool isCorrectLength = idString.Length == 13;
-
-         // Check if all characters are digits
-         bool isAllNumbers = false;
-
-         foreach (char item in idString){
-            isAllNumbers = Char.IsDigit(item);
-         }
-
-         // Check if month is correct
-         bool isMonthCorrect = false;
-
-         int month = Int32.Parse(idString.Substring(2,2));
-         isMonthCorrect = 0 < month && month <= 12;
-
-         // Check if day is correct
-         bool isDayCorrect = false;
-
-         int day = Int32.Parse(idString.Substring(4,2));
-         isDayCorrect = 0 < day && day <= 31;
-
-         return isCorrectLength && isAllNumbers && isMonthCorrect && isDayCorrect;
-      }
-
-      static string createDate(string IdNumber) {
-         string year = IdNumber.Substring(0,2);
-         string month = IdNumber.Substring(2,2);
-         string day = IdNumber.Substring(4,2);
-
-         string dateString = day +"/"+month+"/"+year;
-
-         return dateString;
-      }
-
-      static bool before2010(string idString) {
-         int year = Int32.Parse(idString.Substring(0,2));
-
-         if (year > 23 || year < 10) {
-            return true;
-         }
-         return false;
       }
    }
 }
